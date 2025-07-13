40d5d59922590feecb84aae1e3dd89c1c51f6806
MIGUEL ANGEL DIAZ YANEZ <mikidiaz_10@hotmail.com>
2025-07-13 16:30:50 -0500

    Fix line lenght Flak8

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -14,7 +14,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID'
+                )),
                 ('dni', models.CharField(max_length=13, unique=True)),
                 ('name', models.CharField(max_length=30)),
             ],
